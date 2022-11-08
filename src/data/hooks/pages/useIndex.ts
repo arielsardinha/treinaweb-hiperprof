1801031519b9ebb251638aad080505fc8ff685ec
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-11-08 17:59:42 -0300

    criando lógica da tela home

---
+++ src/data/hooks/pages/useIndex.ts
@@ -0,0 +1,19 @@
+import { useRouter } from "next/router";
+import { FormEvent, useState } from "react";
+
+export default function useIndex() {
+  const router = useRouter(),
+    [search, setSearch] = useState<string>(""),
+    [messageErro, setMessageErro] = useState<string>("");
+
+  function onBuscarProfessor(event: FormEvent) {
+    event.preventDefault();
+    if (search.length >= 3) {
+      router.push("/pesquisa-professor");
+    } else {
+      setMessageErro("minimo de três caracteress");
+    }
+  }
+
+  return { setSearch, messageErro, onBuscarProfessor };
+}
