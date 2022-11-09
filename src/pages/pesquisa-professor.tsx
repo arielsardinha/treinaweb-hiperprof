9d8879db6ed30f106fceba74120458a655e1c137
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-11-09 14:19:12 -0300

    Criando tela da lista de professores

---
+++ src/pages/pesquisa-professor.tsx
@@ -0,0 +1,22 @@
+import PageTitle from "@components/data-display/PageTitle";
+import { TextField, Icon } from "@mui/material";
+
+export default function PesquisaProfessorPage() {
+  return (
+    <>
+      <TextField
+        sx={{ mt: 3, mb: 1 }}
+        label={"Encontre um professor"}
+        InputProps={{
+          startAdornment: <Icon sx={{ mr: 1 }}>search</Icon>,
+        }}
+        fullWidth
+        required
+      />
+      <PageTitle
+        title="Professores Encontrados"
+        subtitle="Clique sobre um professor para ver os detalhes e poder marcar uma aula com o mesmo"
+      />
+    </>
+  );
+}
