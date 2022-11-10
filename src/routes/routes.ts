eaab3dc128376f601e1f98f8689b57254f38dda4
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-11-10 12:22:24 -0300

    configurando roteamento para a tela detalhe do professor

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -15,4 +15,11 @@ export const Router = {
     },
     icon: "",
   },
+  detalheProfessor: {
+    name: "/professor/detalhe-professor",
+    push: function (router: NextRouter) {
+      router.push({ pathname: this.name });
+    },
+    icon: "",
+  },
 };
