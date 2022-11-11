5cab1e04f8778c0710e6ffe1cde1672548660a92
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-11-11 16:21:20 -0300

    Criando tela de detalhe do professor

---
+++ src/data/hooks/pages/professor/useDetalheProfessor.ts
@@ -0,0 +1,109 @@
+import {
+  AlunoErroResponseInterface,
+  AlunoInterface,
+} from "@data/@types/alunos";
+import { ResponseErroInterface } from "@data/@types/axios_response";
+import { ProfessorInterface } from "@data/@types/professor";
+import { ApiService } from "@data/services/ApiService";
+import { BrowserService } from "@data/services/BrowserService";
+import { Router } from "@routes/routes";
+import { AxiosError, AxiosResponse } from "axios";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+export default function useDetalheProfessor() {
+  const router = useRouter(),
+    [professor, setProfessor] = useState<ProfessorInterface>(),
+    [professores, setProfessores] = useState<ProfessorInterface[]>(),
+    [oepnDialog, setOpenDialog] = useState<boolean>(false),
+    [aluno, setAluno] = useState<AlunoInterface>({
+      nome: "",
+      data_aula: "",
+      email: "",
+    }),
+    [snackMessage, setSnackMessage] = useState(""),
+    [alunoErro, setAlunoErro] = useState<AlunoErroResponseInterface>();
+
+  useEffect(() => {
+    const data = sessionStorage.getItem("hiperprof_professor");
+
+    if (data) {
+      setProfessor(JSON.parse(data));
+      getProfessores();
+    } else {
+      Router.home.push(router);
+    }
+
+    // return () => {
+    //   sessionStorage.removeItem("hiperprof_professor");
+    // };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  async function getProfessores() {
+    await ApiService.get("/api/professores", {
+      params: router.query.search,
+    })
+      .then(({ data }: AxiosResponse<ProfessorInterface[]>) => {
+        setProfessores(data);
+      })
+      .catch(() => setProfessores([]));
+  }
+
+  function selecionarProfessor(professor: ProfessorInterface) {
+    setProfessor(professor);
+    sessionStorage.setItem("hiperprof_professor", JSON.stringify(professor));
+    BrowserService.scrolToTop();
+  }
+
+  function formatDataToJson(data: string): Date {
+    // 10/10/2022 15:00
+    // ["10/10/2022", "15:00"]
+    // ["10" ,"10", "2022"]
+    // ["15","00"]
+    const [_data, tempo] = data.split(" "),
+      [dia, mes, ano] = _data.split("/"),
+      [hora, minuto] = tempo?.split(":") ?? [];
+
+    const newDate = new Date(`${mes} ${dia} ${ano} ${hora}:${minuto} UTC`);
+    return newDate;
+  }
+
+  async function handleSubmit() {
+    const newDate = {
+      ...aluno,
+      data_aula: formatDataToJson(aluno.data_aula as string),
+    } as AlunoInterface;
+
+    ApiService.post(`/api/professores/${professor!.id}/alunos`, newDate)
+      .then(() => {
+        setOpenDialog(false);
+        setAluno({ data_aula: "", email: "", nome: "" });
+        setSnackMessage("Agendado com sucesso");
+      })
+      .catch(
+        ({
+          response,
+        }: AxiosError<ResponseErroInterface<AlunoErroResponseInterface>>) => {
+          if (response) {
+            setAlunoErro(response.data.errors);
+            setSnackMessage(response.data.message);
+          }
+        }
+      );
+  }
+
+  return {
+    professor,
+    professores,
+    selecionarProfessor,
+    oepnDialog,
+    setOpenDialog,
+    setAluno,
+    handleSubmit,
+    snackMessage,
+    setSnackMessage,
+    alunoErro,
+    setAlunoErro,
+  };
+}
--- src/pages/professor/detalhe-professor.tsx
+++ src/pages/professor/detalhe-professor.tsx
@@ -1,3 +1,155 @@
+import Fetch from "@components/data-display/Fetch";
+import PageTitle from "@components/data-display/PageTitle";
+import ListaProfessorCard from "@components/data-display/ProfessorCard/listaProfessorCard";
+import Dialog from "@components/feedback/Dialog";
+import useDetalheProfessor from "@data/hooks/pages/professor/useDetalheProfessor";
+import { TextFormatService } from "@data/services/TextFormatService";
+import {
+  Button,
+  Container,
+  Snackbar,
+  TextField,
+  Typography,
+} from "@mui/material";
+import {
+  BoxCardProfessor,
+  BoxDescription,
+  BoxImage,
+} from "@styles/pages/professor/detalhe-professor.stales";
+import InputMask from "react-input-mask";
 export default function DetalheProfessorPage() {
-  return <p>DETALHE DO PROFESSOR</p>;
+  const {
+    professor,
+    professores,
+    selecionarProfessor,
+    oepnDialog,
+    setOpenDialog,
+    setAluno,
+    handleSubmit,
+    snackMessage,
+    setSnackMessage,
+    alunoErro,
+    setAlunoErro,
+  } = useDetalheProfessor();
+  return (
+    <Container>
+      <PageTitle
+        title="Detalhes do professor"
+        subtitle="Veja os detalhes do professor e verifique se ele é o ideal para você se candidatar a uma aula"
+      />
+      <BoxCardProfessor>
+        <BoxImage foto={professor?.foto_perfil} />
+        <BoxDescription>
+          <div className="box_esquerda">
+            <Typography variant="h6">{professor?.nome}</Typography>
+            <Typography
+              sx={{ my: 2 }}
+              className="descricao"
+              paragraph
+              variant="body2"
+            >
+              {professor?.descricao}
+            </Typography>
+          </div>
+          <div className="box_direita">
+            <Typography variant="body2" sx={{ my: 2 }}>
+              PREÇO HORA/AULA
+            </Typography>
+            <Typography variant="h4" paragraph>
+              {TextFormatService.currency(professor?.valor_hora)}
+            </Typography>
+            <Button
+              variant="outlined"
+              color="inherit"
+              onClick={() => setOpenDialog(true)}
+            >
+              Contratar
+            </Button>
+          </div>
+        </BoxDescription>
+      </BoxCardProfessor>
+      <Typography variant="body2" color={"grey"} sx={{ my: 10 }}>
+        {professor?.descricao}
+      </Typography>
+      <Fetch
+        data={professores?.filter(({ id }) => id !== professor?.id)}
+        maxLength={3}
+        render={(professoresFiltrado) => {
+          return (
+            <>
+              <PageTitle
+                title="OUTROS PROFESSORES SUGERIDOS"
+                color={"primary.light"}
+              />
+              <ListaProfessorCard
+                professores={professoresFiltrado}
+                onClick={selecionarProfessor}
+              />
+            </>
+          );
+        }}
+      />
+      <Dialog
+        isOpen={oepnDialog}
+        title={"Preencha suas informações"}
+        onConfirm={handleSubmit}
+        onClose={() => {
+          setOpenDialog(false);
+          setAlunoErro(undefined);
+        }}
+      >
+        <TextField
+          label={"Seu nome"}
+          error={alunoErro?.nome != undefined}
+          helperText={alunoErro?.nome}
+          onChange={({ target: { value } }) => {
+            setAluno((prevState) => {
+              return { ...prevState, nome: value };
+            });
+          }}
+          fullWidth
+        />
+        <TextField
+          label={"Seu Email"}
+          type={"email"}
+          sx={{ my: 3 }}
+          error={alunoErro?.email != undefined}
+          helperText={alunoErro?.email}
+          onChange={({ target: { value } }) => {
+            setAluno((prevState) => {
+              return { ...prevState, email: value };
+            });
+          }}
+          fullWidth
+        />
+        <InputMask
+          mask={"99/99/9999 99:99"}
+          onChange={({ target: { value } }: any) => {
+            setAluno((prevState) => {
+              return { ...prevState, data_aula: value };
+            });
+          }}
+        >
+          {() => {
+            return (
+              <TextField
+                label={"Horário da aula"}
+                error={alunoErro?.data_aula != undefined}
+                helperText={alunoErro?.data_aula as string}
+                fullWidth
+              />
+            );
+          }}
+        </InputMask>
+      </Dialog>
+      <Snackbar
+        open={snackMessage.length > 0}
+        message={snackMessage}
+        autoHideDuration={4000}
+        onClose={() => {
+          setSnackMessage("");
+        }}
+      />
+    </Container>
+  );
 }
